69a1655b678b66e42bcb239ad44d1954fc1ad703
pedrotommaselli@gmail.com <pedrotommaselli@gmail.com>
2020-01-14 12:14:36 -0400

    gobarberweb finalizado

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from './styles';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -13,9 +17,15 @@ export default function Profile() {
     dispatch(updateProfileRequest(data));
   }
 
+  function hadleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
@@ -35,7 +45,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={hadleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
